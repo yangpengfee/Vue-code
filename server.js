51bc3264a4ab63b9350a14914a7c5c7371708301
Your Name <1838740522@qq.com>
2020-09-28 22:28:07 +0800

    添加vitetest

---
+++ server.js
@@ -0,0 +1,82 @@
+const fs = require('fs')
+const path = require("path")
+const Koa = require('koa')
+const compilerSfc = require('@vue/compiler-sfc')
+const compilerDom = require('@vue/compiler-dom')
+
+const app = new Koa()
+
+function rewriteImport(content) {
+    return content.replace(/ from ['|"]([^'"]+)['|"]/g,
+        function (s0, s1) {
+            if (s1[0] !== '.' && s1[1] !== '/') {
+                return `from '/@modules/${s1}'`
+            } else {
+                return s0
+            }
+        })
+}
+app.use(ctx => {
+    const { request: { url, query } } = ctx;
+    if (url === '/') {
+        let content = fs.readFileSync('./index.html', 'utf-8')
+        content = content.replace('<scripte', `
+        <script>
+            window.process = {
+            env: {NODE_EV:'dev'}
+            }
+        </script>
+        <script
+        
+        `)
+        ctx.type = 'text/html'
+        ctx.body = content
+    } else if (url.endsWith('.css')) {
+        const p = path.resolve(__dirname, url.slice(1))
+        const file = fs.readFileSync(p, 'utf-8')
+        const content = `
+        const css= "${file.replace(/\n/g, '')}"
+        const link = document.createElemnet('style')
+        link.setAttribute(type,'text/css')
+        document.head.appendChild(link)
+        link.innerHTML = css
+        export default css
+        `
+        ctx.type = 'application/javascript'
+        ctx.body = content
+    } else if (url.endsWith('.js')) {
+        const p = path.resolve(__dirname, url.slice(1))
+        ctx.type = 'application/javascript'
+        const content = fs.readFileSync(p, 'utf-8')
+        ctx.body = rewriteImport(content);
+    } else if (url.startsWith('/@modules/')) {
+        const prefix = path.resolve(__dirname, 'node_modules', url.replace('/@modules/', ''))
+        const module = require(prefix + '/package.json').module
+        const p = path.resolve(prefix, module)
+        const ret = fs.readFileSync(p, 'utf-8')
+        ctx.type = 'application/javascript'
+        ctx.body = rewriteImport(ret)
+    } else if (url.indexOf('.vue') > -1) {
+        const p = path.resolve(__dirname, url.split("?")[0].slice(1))
+        const { descriptor } = compilerSfc.parse(fs.readFileSync(p, 'utf-8'))
+        if (!query.type) {
+            ctx.type = 'application/javascript'
+            ctx.body = `
+            ${rewriteImport(descriptor.script.content.replace('export default', 'const __script='))}
+            import {render as __render} from "${url}?type=template"
+            __script.render = __render 
+            export default __script
+            `
+        } else if (query.type == "template") {
+            const template = descriptor.template
+            const render = compilerDom.compile(template.content, {
+                mode: "module"
+            }).code
+            ctx.type = "application/javascript"
+            ctx.body = rewriteImport(render)
+        }
+    }
+})
+app.listen(9092, () => {
+    console.log('listen 9092')
+})
